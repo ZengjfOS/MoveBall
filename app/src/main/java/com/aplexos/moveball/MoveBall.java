52e4efa35e597fbfe5b36ee4b8b9339dbf25fed2
zengjf <zengjf42@163.com>
2016-06-13 21:21:57 +0800

    Initial commit

---
+++ app/src/main/java/com/aplexos/moveball/MoveBall.java
@@ -0,0 +1,28 @@
+package com.aplexos.moveball;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by aplex on 16-6-13.
+ */
+public class MoveBall extends View implements View.OnClickListener {
+
+    public MoveBall(Context context) {
+        super(context);
+    }
+
+    public MoveBall(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MoveBall(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
